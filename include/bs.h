f03f5a3517d76e6d34b41e94ab0c0a04181f39f6
Collie <qetup1988+github@gmail.com>
2022-06-03 11:08:49 +0800

    ReWrite partial of FB_string

--- include/bs.h
+++ include/bs.h
@@ -6,12 +6,26 @@
 
 #include "list.h"
 
+typedef struct FB_string FB_string;
+
+struct FB_string {
+	size_t length;
+	char* data;
+};
+
+FB_string* fbs_ctor(const char* const str);
+void fbs_resize(FB_string* str, int new_size);
+FB_string* fbs_combine(const FB_string* const str1, const FB_string* const str2);
+FB_string* fbs_get_random(uint32_t len);
+FB_string* fbs_replace(const FB_string* const origin, const FB_string* const replace_pattern,
+					   const FB_string* const with);
+int fbs_is_same(const FB_string* const str1, const FB_string* const str2);
+
 #define BS_HEADER_LEN 4
 
 char* bsNew(const char*);
 char* bsNewLen(char*, size_t);
 char* bsCat(char*, char*);
-char* bsSubstr(const char*, uint32_t, int32_t);
 char* bsRandom(uint32_t, char*);
 char* bsEscape(const char*);
 char* bsNewline2BR(const char*);
--- src/bs.c
+++ src/bs.c
@@ -4,6 +4,100 @@
 
 #include "bs.h"
 
+FB_string* fbs_ctor(const char* const str) {
+	size_t len = strlen(str);
+	FB_string* ret = malloc(sizeof(FB_string));
+	ret->data = malloc(sizeof(char) * (len + 1));
+	ret->length = len;
+	strncpy(ret->data, str, len + 1);
+}
+
+void fbs_resize(FB_string* str, int new_size) {
+	str->data = realloc(str->data, sizeof(char) * (new_size + 1));
+	str->length = new_size;
+}
+
+FB_string* fbs_combine(const FB_string* const str1, const FB_string* const str2) {
+	FB_string* ret = malloc(sizeof(FB_string));
+	ret->length = str1->length + str2->length;
+	ret->data = malloc(sizeof(char) * (str1->length + str2->length + 1));
+	strncpy(ret, str1, str1->length);
+	strncpy(ret + str1->length, str2, str2->length + 1);
+	return ret;
+}
+
+FB_string* fbs_get_random(uint32_t len) {
+	static const char table[] = { '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'a', 'b', 'c',
+								  'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p',
+								  'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C',
+								  'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P',
+								  'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z' };
+	char random[len + 1];
+	for(int i = 0; i < len; i++)
+		random[i] = table[rand() % sizeof(table)];
+	random[len] = '\0';
+	FB_string* ret = fbs_ctor(random);
+	return ret;
+}
+
+FB_string* fbs_replace(const FB_string* const origin, const FB_string* const replace_pattern,
+					   const FB_string* const with) {
+	FB_string* cpy_origin = fbs_ctor(origin->data);
+	/* copied, pasted, and modified from stack overflow:
+	 * https://stackoverflow.com/a/779960/14835280 */
+	// sanity checks and initialization
+	if(!origin || !replace_pattern || !replace_pattern->length)
+		return NULL;
+	const FB_string* const empty = fbs_ctor("");
+	const FB_string* const new_with = with ? with : empty;
+	char* tmp;								
+	int count = 0;							// number of replacements
+	char* insert_point = cpy_origin->data;	// the next insert point
+	// count the number of replacements needed
+	for(count = 0; tmp = strstr(insert_point, replace_pattern->data); ++count)
+		insert_point = tmp + replace_pattern->length;
+	char* result = tmp =
+		malloc(sizeof(char)
+			   * (cpy_origin->length + (new_with->length - replace_pattern->length) * count + 1));
+	if(!result)
+		return NULL;
+
+	// first time through the loop, all the variable are set correctly
+	// from here on,
+	// tmp points to the end of the result string
+	// insert_point points to the next occurrence of rep in orig
+	// orig points to the remainder of orig after "end of rep"
+	// len_front: distance between rep and end of last rep
+	for(int len_front;; count--) {
+		insert_point = strstr(cpy_origin->data, replace_pattern->data);
+		len_front = insert_point - cpy_origin->data;
+		tmp = strncpy(tmp, cpy_origin->data, len_front) + len_front;
+		tmp = strcpy(tmp, new_with) + new_with->length;
+		cpy_origin->data += len_front + replace_pattern->length;  // move to next "end of rep"
+	}
+	strcpy(tmp, cpy_origin->data);
+	fbs_dtor(cpy_origin);
+	fbs_dtor(empty);
+	const FB_string* const ret = fbs_ctor(result);
+	free(result);
+	return ret;
+}
+
+void fbs_dtor(FB_string* str) {
+	if(str->data)
+		free(str->data);
+	free(str);
+}
+
+int fbs_is_same(const FB_string* const str1, const FB_string* const str2) {
+	char *elem1 = str1->data, *elem2 = str2->data;
+	for(; elem1 && elem2; elem1++, elem2++) {
+		if(*elem1 != *elem2)
+			return false;
+	}
+	return elem1 == elem2;
+}
+
 char* bsNew(const char* str) {
 	size_t len = strlen(str);
 	char* bs = malloc(sizeof(char) * (BS_HEADER_LEN + len + 1));
@@ -40,24 +134,6 @@ char* bsCat(char* bs1, char* bs2) {
 	strncpy(bs + len1, bs2, len2);
 
 	bsSetLen(bs, len);
-
-	return bs;
-}
-
-char* bsSubstr(const char* orig, uint32_t beginning, int32_t end) {
-	size_t len = bsGetLen(orig);
-	size_t newLen = (end <= 0) ? len - beginning + end : end - beginning;
-
-	assert(newLen > 0);
-	assert(newLen <= len);
-
-	char* bs = malloc(sizeof(char) * (BS_HEADER_LEN + newLen + 1));
-	assert(bs);
-	bs += BS_HEADER_LEN;
-
-	memcpy(bs, orig + beginning, newLen);
-
-	bsSetLen(bs, newLen);
 
 	return bs;
 }
