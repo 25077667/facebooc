919fd9a0d1668e3bcc03de1eb58d99b0076fa491
25077667 <zxc25077667@gmail.com>
2023-01-03 02:09:37 +0800

    Remove insecure function

--- main.c
+++ main.c
@@ -6,6 +6,15 @@
 static void sig(int signum);
 Facebooc *fb;
 
+static inline _Bool is_number(const char *str)
+{
+    for (int i = 0; str[i] != '\0'; i++) {
+        if (str[i] < '0' || str[i] > '9')
+            return 0;
+    }
+    return 1;
+}
+
 int main(int argc, char *argv[])
 {
     if (signal(SIGINT, sig) == SIG_ERR || signal(SIGTERM, sig) == SIG_ERR) {
@@ -15,11 +24,13 @@ int main(int argc, char *argv[])
 
     uint16_t server_port = 8080;
 
-    if (argc > 1 && sscanf(argv[1], "%hu", &server_port) == 0) {
-        fprintf(stderr,
-                "error: invalid command line argument, using default port "
-                "8080.\n");
-        server_port = 8080;
+    // parse argv[1] as server port
+    if (argc > 1) {
+        if (!is_number(argv[1])) {
+            fprintf(stderr, "error: invalid port number\n");
+            return 1;
+        }
+        server_port = atoi(argv[1]);
     }
 
     fb = FB_new(server_port);
